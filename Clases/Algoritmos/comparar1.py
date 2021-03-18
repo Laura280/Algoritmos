a90cc513c4d9c4c0250bac8593c94345d42ad3b6
laura280 <lauramontoyac.01@gmail.com>
2021-03-18 13:25:44 -0500

    clase de ordenamiento de burbuja, comparación de sort y OB

---
+++ Clases/Algoritmos/comparar1.py
@@ -0,0 +1,26 @@
+import time
+import ordenamiento_burbuja as ob
+import random as r
+
+lista = []
+for i in range (1200):
+    lista.append(r.randint (1,10000))
+listaCopia = lista.copy()
+
+#--- INICIO ---#
+inicio = time.time()
+#---INSTRUCCIONES ---#
+ob.ordenamientoBurbuja(lista)
+#--- DELTA ---#
+deltaOb = time.time() - inicio
+
+#--- INICIO ---#
+inicio = time.time()
+#---INSTRUCCIONES ---#
+listaCopia.sort()
+#--- DELTA ---#
+deltaSort = time.time() - inicio
+
+print(deltaOb)
+print(deltaSort)
+print(deltaSort >= deltaOb)
